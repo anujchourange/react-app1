c9c733c9d4b7be19c041ffba3e8fcf18aec94a30
Anuj <35633590+IOanuj@users.noreply.github.com>
2020-06-29 00:09:43 +0530

    implemented loading spinner

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react'
 import UserItem from './UserItem'
-
-export class Users extends Component {
+import Spinner from '../layout/Spinner'
+const Users = ({users, loading}) => {
    
-    render() {
+    if (loading){
+
+        return <Spinner /> 
+    }
+    else{
         return (
             <div style={userStyle}>
-               {this.props.users.map(user =>(
-                 <UserItem key={user.id} user={user}/>
-               ))} 
+                {users.map(user =>(
+                    <UserItem key={user.id} user={user}/>
+                ))} 
             </div>
         )
-    }
+   }
+
 }
 
-const userStyle={
+const userStyle={ 
     display:'grid',
     gridTemplateColumns:'repeat(3,1fr)',
     gridGap:'1rem',
